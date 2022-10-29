7134a122a4729f7d266bb98019150a34c254548b
Dominic Watts <dominic.watts@hotmail.co.uk>
2022-10-29 15:39:52 +0100

    deploy

---
+++ deploy.php
@@ -0,0 +1,22 @@
+<?php
+namespace Deployer;
+
+require 'recipe/magento2.php';
+
+// Config
+
+set('repository', 'git@github.com:DominicWatts/Mage243.git');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('127.0.0.1')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/app');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
